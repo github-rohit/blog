3f81e4219cc5bc3409f4ef9384c05fbf167704af
Rohit Kumar Gautam <nirmal.rohit@yahoo.co.in>
2017-10-09 20:12:54 +0530

    ui update and added capture code

--- config/index.js
+++ config/index.js
@@ -2,6 +2,7 @@ var config = require('./config');
 
 module.exports = {
     getDbConnectionString: function(){
+		//return 'mongodb://localhost/blog';
         return 'mongodb://' + config.uname + ':' + config.pwd + '@ds161574.mlab.com:61574/blog';
     }
 }
--- public/app/register/register.controller.js
+++ public/app/register/register.controller.js
@@ -1,15 +1,28 @@
 (function(app){
 
 	app.module.controller('RegisterController', RegisterController);
-		RegisterController.$inject = ['$http', 'FormErrorService', 'RegisterService']
+		RegisterController.$inject = ['$scope', '$http', 'FormErrorService', 'RegisterService']
 
-		function RegisterController($http, FormErrorService, RegisterService) {
+		function RegisterController($scope, $http, FormErrorService, RegisterService) {
 			const self = this;
-			this.formData = {};
+			this.formData = {
+				gender: ''
+			};
 			this.formError = {};
 			this.formValidateMsg = FormErrorService.messages;
+			this.userSuccess = false;
 
+			grecaptcha.render('gre-captcha', {
+			  'sitekey' : '6LciizMUAAAAAE0nWYFK33EmsDQ_L0M7TwyHULvz'
+			});
+			
 			this.submit = function() {
+				var g = grecaptcha.getResponse();
+				if (!g) {
+					alert("The reCAPTCHA wasn't entered correctly.");
+					return;
+				}
+				
 				RegisterService.register(self.formData, function(res){
 					const data = res.data || {};
 					self.emailunameTaken = {};
