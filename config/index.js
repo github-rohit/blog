319b1bf70d5fe1d9a5cbced4e85b0dbb890a93e5
Rohit Kumar Gautam <nirmal.rohit@yahoo.co.in>
2017-11-05 19:00:49 +0530

    remove file

--- config/index.js
+++
@@ -1,8 +0,0 @@
-var config = require('./config');
-
-module.exports = {
-    getDbConnectionString: function(){
-		//return 'mongodb://localhost/blog';
-        return 'mongodb://' + config.uname + ':' + config.pwd + '@ds161574.mlab.com:61574/blog';
-    }
-}
